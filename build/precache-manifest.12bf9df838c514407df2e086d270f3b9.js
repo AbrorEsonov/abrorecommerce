8d96a534b62fe53155ee6dec30df507c716cb1b1
AbrorEsonov <abroresonov5@gmail.com>
2020-09-24 12:23:56 +0500

    git chnged

---
+++ build/precache-manifest.12bf9df838c514407df2e086d270f3b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0f8073b596527c3e92c6177d4fbf41",
+    "url": "/AbrorEsonov/abrorglamourbandixon/index.html"
+  },
+  {
+    "revision": "39e88b9d0961a07d2fb1",
+    "url": "/AbrorEsonov/abrorglamourbandixon/static/css/main.5b47a4dc.chunk.css"
+  },
+  {
+    "revision": "d4d4c94ac91c40474c0d",
+    "url": "/AbrorEsonov/abrorglamourbandixon/static/js/2.e06ed0ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AbrorEsonov/abrorglamourbandixon/static/js/2.e06ed0ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39e88b9d0961a07d2fb1",
+    "url": "/AbrorEsonov/abrorglamourbandixon/static/js/main.5c05bc91.chunk.js"
+  },
+  {
+    "revision": "8339052880b29f07725e",
+    "url": "/AbrorEsonov/abrorglamourbandixon/static/js/runtime-main.edfce60d.js"
+  }
+]);
