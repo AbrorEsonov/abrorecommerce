1a3ed83045136e79db5438388bc58996c931f5da
AbrorEsonov <abroresonov5@gmail.com>
2020-09-24 10:52:47 +0500

    changed

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import productReducer from "./productReducer";
-import cartReducers from "./cartReducers";
-export default combineReducers({
-    products: productReducer,
-    cart: cartReducers
-})
